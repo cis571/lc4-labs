1cc3b2344addb02273013ea2fc7ab55d448cb9b3
Joe Devietti <devietti@cis.upenn.edu>
2022-01-14 23:43:38 -0500

    added CLA template to Lab 4

---
+++ lab4-pipeline/lc4_cla.v
@@ -0,0 +1,60 @@
+/* TODO: INSERT NAME AND PENNKEY HERE */
+
+`timescale 1ns / 1ps
+`default_nettype none
+
+/**
+ * @param a first 1-bit input
+ * @param b second 1-bit input
+ * @param g whether a and b generate a carry
+ * @param p whether a and b would propagate an incoming carry
+ */
+module gp1(input wire a, b,
+           output wire g, p);
+   assign g = a & b;
+   assign p = a | b;
+endmodule
+
+/**
+ * Computes aggregate generate/propagate signals over a 4-bit window.
+ * @param gin incoming generate signals 
+ * @param pin incoming propagate signals
+ * @param cin the incoming carry
+ * @param gout whether these 4 bits collectively generate a carry (ignoring cin)
+ * @param pout whether these 4 bits collectively would propagate an incoming carry (ignoring cin)
+ * @param cout the carry outs for the low-order 3 bits
+ */
+module gp4(input wire [3:0] gin, pin,
+           input wire cin,
+           output wire gout, pout,
+           output wire [2:0] cout);
+   
+endmodule
+
+/**
+ * 16-bit Carry-Lookahead Adder
+ * @param a first input
+ * @param b second input
+ * @param cin carry in
+ * @param sum sum of a + b + carry-in
+ */
+module cla16
+  (input wire [15:0]  a, b,
+   input wire         cin,
+   output wire [15:0] sum);
+
+endmodule
+
+
+/** Lab 2 Extra Credit, see details at
+  https://github.com/upenn-acg/cis501/blob/master/lab2-alu/lab2-cla.md#extra-credit
+ If you are not doing the extra credit, you should leave this module empty.
+ */
+module gpn
+  #(parameter N = 4)
+  (input wire [N-1:0] gin, pin,
+   input wire  cin,
+   output wire gout, pout,
+   output wire [N-2:0] cout);
+ 
+endmodule
